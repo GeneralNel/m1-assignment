62f1e4a1f02bf4cce060fcbbd3d07310c5797ca4
Mushfiq Rashid <mushfiqarashid@gmail.com>
2025-09-28 20:55:07 -0700

    update contact form to add dropdown logic

--- js/dropdown_scripts.js
+++ js/dropdown_scripts.js
@@ -1,3 +1,20 @@
 var dropdownElement = document.getElementById("contactMethod");
 var emailInput = document.getElementById("emailInput");
 var phoneInput = document.getElementById("phoneInput");
+
+dropdownElement.addEventListener("change", showContactInput);
+
+function showContactInput(event) {
+    var selectedValue = event.target.value;
+    console.log(selectedValue);
+    if (selectedValue == "email") {
+        emailInput.style.visibility = "visible";
+        phoneInput.style.visibility = "hidden";
+    } else if (selectedValue == "phone") {
+        emailInput.style.visibility = "hidden";
+        phoneInput.style.visibility = "visible";
+    } else {
+        emailInput.style.visibility = "hidden";
+        phoneInput.style.visibility = "hidden";
+    }
+}
